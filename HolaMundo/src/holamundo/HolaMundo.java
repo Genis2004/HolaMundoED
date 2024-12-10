84c2b493702bebcb0e57e16dba633a46913f7ca9
genis <grodriguez19012@iesjoanramis.org>
2024-12-10 13:01:51 +0100

    en rama desarollo

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,11 @@
+package holamundo;
+import java.util.Scanner;
+public class HolaMundo {
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Ingresa tu nombre: ");
+String nombre = scanner.nextLine();
+System.out.println("¡Hola, " + nombre + "! Bienvenido a Java ptimer cambio.");
+}
+//en rama desarollo
+}
